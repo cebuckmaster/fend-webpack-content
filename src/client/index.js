5a1d96738bb6beb0374178f03f9bfb78e85bdb7d
Carl Buckmaster <cebuckmaster@gmail.com>
2020-02-15 18:37:14 +0000

    json test

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import { handleSubmit } from './js/formHandler';
+import { nameChecker } from './js/nameChecker';
+import json from './test.json';
+
+
+alert("I exist");
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: '/\.json$/',
+                loader: "json-loader"
+            }
+        ]
+    }
+}
+
+
