4a935703bc38b6e68f3d682690bc3c7352fffc08
Carl Buckmaster <cebuckmaster@gmail.com>
2020-02-15 19:46:45 +0000

    final update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     module: {
         rules: [
@@ -16,7 +19,14 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/client/views/index.html',
             filename: './index.html'
-        })
+        }),
+        new CleanWebpackPlugin({
+            dry: true,
+            verbose: true,
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
 
