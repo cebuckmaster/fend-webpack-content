6524003628e17c1c30622bf8bc7b34f67cd19296
Carl Buckmaster <cebuckmaster@gmail.com>
2020-02-15 18:42:45 +0000

    back to work

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,7 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: '/\.json$/',
-                loader: "json-loader"
-            }
-        ]
+            }        ]
     }
 }
 
