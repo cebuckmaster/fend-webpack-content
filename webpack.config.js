fae815b392d4e6d787d69b75a742dd06370dc70f
Carl Buckmaster <cebuckmaster@gmail.com>
2020-02-15 19:11:23 +0000

    html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -10,7 +11,13 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }        ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
 }
 
 
